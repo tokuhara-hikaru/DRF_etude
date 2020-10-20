9e54612fb35abfdcb29a6a778b35d8a52243ea93
tokuhara-hikaru <tokuhara.hikaru@isid.co.jp>
2020-10-20 14:28:49 +0900

    venv削除

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\li3548\desktop\drf_1\drf-simplest-sample\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
